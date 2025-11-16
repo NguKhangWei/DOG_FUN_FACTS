7c3b562aa42db851c07215a63ae31c93c5f72d10
Ngu Khang Wei <kngu0114@student.monash.edu>
2025-11-16 10:12:02 +0800

    Initial file setup

---
+++ src/main/java/com/khangwei/dogfunfacts/DogfunfactsApplication.java
@@ -0,0 +1,13 @@
+package com.khangwei.dogfunfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogfunfactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogfunfactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khangwei/dogfunfacts/DogfunfactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khangwei.dogfunfacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogfunfactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
